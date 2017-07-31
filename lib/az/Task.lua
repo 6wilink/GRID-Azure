40f49e2f8fb1fe5b5413c6fc6b7300b79596e427
Qige Zhao <zhaoqige@users.noreply.github.com>
2017-07-31 10:29:35 +0800

    Azure IoTHub (via HTTPS) (#11)
    * Update due to "grid-base" upgrade to "qz-grid"
    * Fix json wrong format
    * Submit via HTTPs (sample)

--- lib/az/Task.lua
+++ lib/az/Task.lua
@@ -5,10 +5,14 @@ local cmd = require 'six.cmd'
 local fmt = require 'six.fmt'
 local file = require 'six.file'
 
+local _print = io.write
 local _echo = fmt.echo
 local _sleep = cmd.sleep
 local _save = file.write
 local _read = file.read
+
+local _exec = cmd.exec
+local _fmt = string.format
 
 
 local kpi = require 'az.KPI'
@@ -16,6 +20,17 @@ local mqtt = require 'az.MQTT'
 
 
 local Task = {}
+
+Task.device = {}
+Task.device._SASToken = ''
+Task.device._host = 'GWS-HUB.azure-devices.net'
+Task.device._sig = 'yvnof6SMr%2FDKRH1m1a8tfLKpYUoPi5Tbli3qJo3YyEc%3D'
+
+Task.IotHub = {}
+Task.IotHub._fmt_req1 = 'curl -k --data \''
+Task.IotHub._fmt_req2 = '\' --header "Authorization: SharedAccessSignature sr=GWS-HUB.azure-devices.net%2Fdevices%2Fdevice1&sig=%s&se=1522777587"'
+	.. ' https://GWS-HUB.azure-devices.net/devices/device1/messages/events?api-version=2016-11-14'
+
 
 Task.conf = {}
 Task.conf._SIGNAL = '/tmp/.grid_azure_signal'
@@ -36,8 +51,11 @@ end
 -- instant update (abb/nw)
 -- its coroutine update delayed (gws/sys)
 function Task.Run(mode)
-	local _mqtt_data
+	local _json_data
 	local data_instant, data_delayed
+
+	local _iothub_req_fmt1 = Task.IotHub._fmt_req1
+	local _iothub_req_fmt2 = Task.IotHub._fmt_req2
 
 	while(Task.flag._SIGNAL()) do
 		local ts = os.time()
@@ -49,10 +67,15 @@ function Task.Run(mode)
 		data_instant = kpi.update.Instant()
 
 		_echo('> encoding kpi ...\n')
-		_mqtt_msg = Task.data.encode(data_instant)
+		_json_data = Task.data.encode(data_instant)
 
-		_echo('> sending via MQTT ...\n')
-		_echo('> reading MQTT cmd ...\n')
+		--_echo('> sending via MQTT ...\n')
+		_echo('> sending via HTTPs ...\n')
+		local _iothub_request = _iothub_req_fmt1 .. _json_data .. _iothub_req_fmt2
+		_exec(_iothub_request)
+		_print(_iothub_request)
+
+		_echo('. reading MQTT cmd ...\n')
 		_echo('-------- -------- idle (%d) ------\n', ts)
 
 		_sleep(1)
@@ -108,7 +131,7 @@ function Task.data.encode(data_instant, data_delayed)
 		--if (data_instant[abb])
 		local _fmt = Task.conf.fmt.nw
 		local _nw_data = data_instant.nw
-		_nw = string.format(_fmt, _nw_data.lan_ip or '-', _nw_data.wan_ip or '-', 
+		_nw = string.format(_fmt, _nw_data.lan_ip or '-', _nw_data.wan_ip or '-',
 			_nw_data.eth_rxb or 0, _nw_data.eth_txb or 0)
 	end
 
@@ -116,9 +139,9 @@ function Task.data.encode(data_instant, data_delayed)
 	_ts = string.format(_ts_fmt, os.date("%Y-%m-%d %X"), os.time())
 
 	local _result_fmt = Task.conf.fmt.all
-	_result = string.format(_result_fmt, _ts or 'null', _abb or 'null', _nw or 'null', 
-		_gws or 'null', _nw or 'null') 
-	
+	_result = string.format(_result_fmt, _ts or 'null', _abb or 'null', _nw or 'null',
+		_gws or 'null', _nw or 'null')
+
 	_echo('msg> %s\n', _result)
 	return _result
 end
